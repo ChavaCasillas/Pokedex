671bc017c3f3a052ca0d93c4065ebb7a76d3d637
Chava Casillas <salvadorcasillasmdelc@gmail.com>
2026-02-27 21:45:20 -0600

    Adding more atributes to the class Pokemon, with their respective types of @dataclass as need it, atributes added are: abilities, moves, stats, sprites and cries

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,7 @@
 import json
 
 import pokedex.cli as cli
-from pokedex.client.models import Pokemon
+from pokedex.client.models import NamedAPIResource, Pokemon, PokemonMove
 
 
 def test_get_pokemon_human_output(monkeypatch, capsys):
@@ -11,7 +11,17 @@ def test_get_pokemon_human_output(monkeypatch, capsys):
 
     ## 1. Fake get_pokemon method that returns a predefined Pokemon object for testing purposes
     def fake_get_pokemon(self, name_or_id: str) -> Pokemon:
-        return Pokemon(id=1, name="bulbasaur", types=["grass", "poison"])
+        return Pokemon(
+            id=1,
+            name="bulbasaur",
+            types=["grass", "poison"],
+            moves=[
+                PokemonMove(NamedAPIResource("razor-wind", "url")),
+                PokemonMove(NamedAPIResource("swords-dance", "url")),
+                PokemonMove(NamedAPIResource("cut", "url")),
+                PokemonMove(NamedAPIResource("bind", "url")),
+            ],
+        )
 
     ## 2. Use monkeypatch to replace the get_pokemon method of PokeApiClient with the fake
     # implementation.
@@ -33,13 +43,28 @@ def test_get_pokemon_human_output(monkeypatch, capsys):
     assert "Types" in captured.out
     assert "grass" in captured.out
     assert "poison" in captured.out
+    assert "Moves" in captured.out
+    assert "razor-wind" in captured.out
+    assert "swords-dance" in captured.out
+    assert "cut" in captured.out
+    assert "bind" in captured.out
     assert captured.err == ""
 
 
 def test_get_json_output_success(monkeypatch, capsys):
     # Fake response (sin HTTP real)
     def fake_get_pokemon(self, name_or_id: str):
-        return Pokemon(id=7, name="squirtle", types=["water"])
+        return Pokemon(
+            id=7,
+            name="squirtle",
+            types=["water"],
+            moves=[
+                PokemonMove(NamedAPIResource("mega-punch", "url")),
+                PokemonMove(NamedAPIResource("ice-punch", "url")),
+                PokemonMove(NamedAPIResource("mega-kick", "url")),
+                PokemonMove(NamedAPIResource("headbutt", "url")),
+            ],
+        )
 
     ## Utilizamos monkeypatch para reemplazar el método get_pokemon del PokeApiClient con
     # la función fake_get_pokemon, que devuelve un objeto Pokemon predefinido para las
@@ -63,6 +88,6 @@ def test_get_json_output_success(monkeypatch, capsys):
     assert payload["id"] == 7
     assert payload["name"] == "squirtle"
     assert payload["types"] == ["water"]
-
+    assert payload["moves"] == ["mega-punch", "ice-punch", "mega-kick", "headbutt"]
     # No debe haber errores
     assert captured.err == ""
